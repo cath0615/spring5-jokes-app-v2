dfb4822ceeebb3386ae01f13ac0182305b21786a
cath0615 <md0615>
2021-07-16 16:07:39 +0800

    create spring boot project

---
+++ src/test/java/catherine/demo/spring5jokesApp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package catherine.demo.spring5jokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
